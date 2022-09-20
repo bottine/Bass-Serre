5a15876e099a1e7e826bece8602262bc5a3a420a
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-09-20 18:07:01 +0200

    free groupoid via quotients but very ugly

--- src/free_groupoid.lean
+++ src/free_groupoid.lean
@@ -8,14 +8,9 @@ import data.set.lattice
 import combinatorics.quiver.connected_component
 import group_theory.subgroup.basic
 
-/-
-path_category == the free category of pats
-quotient == quotienting morphisms by relations
-algebra.hom.equiv to use ≃*
--/
 
 open set
-open classical function
+open classical function relation
 local attribute [instance] prop_decidable
 
 
@@ -23,8 +18,10 @@ namespace category_theory
 namespace groupoid
 namespace free
 
-variable {V : Type*}
-variable (Q : quiver V)
+universes u v 
+
+variable {V : Type u}
+variable (Q : quiver.{v+1} V)
 
 inductive word : V → V → Sort*
 | nil {c : V} : word c c
@@ -38,139 +35,140 @@ def word.length : Π {c d : V}, word Q c d → ℕ
 | _ _ (word.cons_p _ t) := t.length.succ
 | _ _ (word.cons_n _ t) := t.length.succ 
 
-lemma word.length_cast : Π {c c' d : V} (w : word Q c d) (e : c = c'), w.length = (e.rec_on w).length :=
-by {rintros _ _ _ w e, induction e, simp, }
+@[pattern]
+def letter_p {c d : V} (p : Q.hom c d) : word Q c d := (word.cons_p p word.nil)
+@[pattern]
+def letter_n {c d : V} (p : Q.hom c d) : word Q d c := (word.cons_n p word.nil)
 
-@[pattern]
-def word.letter_p {c d : V} (p : Q.hom c d) : word Q c d := (word.cons_p p word.nil)
-@[pattern]
-def word.letter_n {c d : V} (p : Q.hom c d) : word Q d c := (word.cons_n p word.nil)
+notation ` +[ ` p ` ] ` := letter_p p
+notation ` -[ ` p ` ] ` := letter_n p
 
 def word.append  : Π {c d e : V}, word Q c d → word Q d e → word Q c e
 | _ _ _ (word.nil) w := w
 | _ _ _ (word.cons_p p u) w := word.cons_p p (u.append w)
 | _ _ _ (word.cons_n p u) w := word.cons_n p (u.append w)
 
+
+@[simp] lemma word.nil_append {c d : V} {p : word Q c d} : word.nil.append p = p := sorry
+@[simp] lemma word.append_nil {c d : V} {p : word Q c d} : p.append word.nil = p := sorry
+
 @[simp] lemma word.append_assoc {c d e f : V} {p : word Q c d} {q : word Q d e} {r : word Q e f} : (p.append q).append r = p.append (q.append r) := sorry
 
-infix ` ≫* `:50 := word.append
+infix ` ≫* `:100 := word.append
 
 def word.reverse : Π {c d : V}, word Q c d → word Q d c
 | _ _ (word.nil) := word.nil
-| _ _ (word.cons_p p u) := (u.reverse.append (word.letter_n p))
-| _ _ (word.cons_n p u) := (u.reverse.append (word.letter_p p))
+| _ _ (word.cons_p p u) := (u.reverse.append (letter_n p))
+| _ _ (word.cons_n p u) := (u.reverse.append (letter_p p))
 
-def word.is_head_reduced : Π {c d : V}, word Q c d → Prop 
-| _ _ (word.nil) := true
-| _ _ (word.letter_p _) := true 
-| _ _ (word.letter_n _) := true 
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := ¬ (∃ e : x = w, e.rec_on p = q)
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) := ¬ (∃ e : x = w, e.rec_on p = q)
-| _ _ (word.cons_p p (word.cons_p q tail)) := true
-| _ _ (word.cons_n p (word.cons_n q tail)) := true
+def red_step {c d : V} (p : word Q c d) (q : word Q c d) : Prop :=
+  (∃ (a b : V) (q₀ : word Q c a) (q₁ : word Q a d) (f : Q.hom a b), p = q₀ ≫* +[ f ] ≫* -[ f ] ≫* q₁ ∧ q = q₀ ≫* q₁)
+∨ (∃ (a b : V) (q₀ : word Q c a) (q₁ : word Q a d) (f : Q.hom b a), p = q₀ ≫* -[ f ] ≫* +[ f ] ≫* q₁ ∧ q = q₀ ≫* q₁)
 
-def word.is_reduced : Π {c d : V}, word Q c d → Prop 
-| _ _ (word.nil) := true
-| _ _ (word.letter_p _) := true 
-| _ _ (word.letter_n _) := true 
-| _ _ (word.cons_p p (word.cons_n q tail)) := (word.cons_p p (word.cons_n q tail)).is_head_reduced ∧ (word.cons_n q tail).is_reduced
-| _ _ (word.cons_n p (word.cons_p q tail)) := (word.cons_n p (word.cons_p q tail)).is_head_reduced ∧ (word.cons_p q tail).is_reduced
-| _ _ (word.cons_p p (word.cons_p q tail)) := (word.cons_p p (word.cons_p q tail)).is_head_reduced ∧ (word.cons_p q tail).is_reduced
-| _ _ (word.cons_n p (word.cons_n q tail)) := (word.cons_n p (word.cons_n q tail)).is_head_reduced ∧ (word.cons_n q tail).is_reduced
-
-noncomputable def word.head_reduce : Π {c d : V}, word Q c d → word Q c d
-| _ _ (word.nil) := word.nil
-| _ _ (word.letter_p p) := word.letter_p p 
-| _ _ (word.letter_n p) := word.letter_n p
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := 
-  if h : (∃ e : x = w, e.rec_on p = q) then eq.rec_on h.some.symm tail else (word.cons_p p (word.cons_n q tail))
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) := 
-  if h : (∃ e : x = w, e.rec_on p = q) then eq.rec_on h.some.symm tail else (word.cons_n p (word.cons_p q tail))
-| _ _ (word.cons_p p (word.cons_p q tail)) := (word.cons_p p (word.cons_p q tail))
-| _ _ (word.cons_n p (word.cons_n q tail)) := (word.cons_n p (word.cons_n q tail))
-
-noncomputable def word.reduce : Π {c d : V}, word Q c d → word Q c d
-| _ _ (word.nil) := word.nil
-| _ _ (word.letter_p p) := word.letter_p p 
-| _ _ (word.letter_n p) := word.letter_n p
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := 
-  if h : (∃ e : x = w, e.rec_on p = q) then eq.rec_on h.some.symm tail.reduce else (word.cons_p p (word.cons_n q tail).reduce).head_reduce
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) := 
-  if h : (∃ e : x = w, e.rec_on p = q) then eq.rec_on h.some.symm tail.reduce else (word.cons_n p (word.cons_p q tail).reduce).head_reduce
-| _ _ (word.cons_p p (word.cons_p q tail)) := (word.cons_p p (word.cons_p q tail).reduce).head_reduce
-| _ _ (word.cons_n p (word.cons_n q tail)) := (word.cons_n p (word.cons_n q tail).reduce).head_reduce
-
-postfix ` ↓ `:100 := word.reduce
-
-lemma word.is_head_reduced_iff_head_reduce_eq : Π {c d : V} (w : word Q c d), w.is_head_reduced ↔ w.head_reduce = w
-| _ _ (word.nil) := by {dsimp [word.head_reduce,word.is_head_reduced], simp only,}
-| _ _ (word.letter_p _) := by {dsimp [word.head_reduce,word.is_head_reduced], simp,}
-| _ _ (word.letter_n _) := by {dsimp [word.head_reduce,word.is_head_reduced], simp,}
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := 
-  by 
-  { dsimp [word.head_reduce,word.is_head_reduced], 
-    simp only [not_exists, dite_eq_right_iff, forall_exists_index], 
-    split, 
-    { rintros h₁ h₂ h₃, exact (h₁ h₂ h₃).elim, }, 
-    { rintro h₁ h₂ h₃, specialize h₁ h₂ h₃,
-     let := congr_arg word.length h₁, rw ←word.length_cast at this, dsimp [word.length] at this, sorry} }
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) :=
-  by 
-  { dsimp [word.head_reduce,word.is_head_reduced], 
-    simp only [not_exists, dite_eq_right_iff, forall_exists_index], 
-    split, 
-    { rintros h₁ h₂ h₃, exact (h₁ h₂ h₃).elim, }, 
-    { rintro h₁ h₂ h₃, specialize h₁ h₂ h₃, sorry, } } -- h₁ should be a contradiction, but not sure of the proper way to get it
-| _ _ (word.cons_p p (word.cons_p q tail)) := by {dsimp [word.head_reduce,word.is_head_reduced], simp,}
-| _ _ (word.cons_n p (word.cons_n q tail)) := by {dsimp [word.head_reduce,word.is_head_reduced], simp,}
+def free_groupoid (V : Type u) := V
 
 
-lemma word.is_reduced_iff_reduce_eq : Π {c d : V} (w : word Q c d), w.is_reduced ↔ w.reduce = w
-| _ _ (word.nil) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], simp only,}
-| _ _ (word.letter_p _) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], simp,}
-| _ _ (word.letter_n _) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], simp,}
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := by 
-  { dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], 
-    sorry, }
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) :=
-  by 
-  { dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry, }  -- h₁ should be a contradiction, but not sure of the proper way to get it
-| _ _ (word.cons_p p (word.cons_p q tail)) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry,}
-| _ _ (word.cons_n p (word.cons_n q tail)) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry,}
+@[simp]
+lemma red_step.reverse  {c d : V} (p₀ p₁ : word Q c d) : red_step p₀.reverse p₁.reverse ↔ red_step p₀ p₁ := sorry 
 
-lemma word.reduce_is_reduced: Π {c d : V} (w : word Q c d), w.reduce.is_reduced := sorry
+@[simp]
+lemma red_step.append_left_congr  {c d e : V} {p₀ p₁ : word Q c d} {q : word Q d e} : red_step p₀ p₁ → red_step (p₀ ≫* q) (p₁ ≫* q) :=
+begin sorry end
 
-lemma word.reduce_idem {c d : V} (w : word Q c d) : w.reduce.reduce = w.reduce := 
-begin 
-  rw ←word.is_reduced_iff_reduce_eq (w.reduce),
-  apply word.reduce_is_reduced,
+@[simp]
+lemma red_step.append_right_congr  {c d e : V} {p : word Q c d} {q₀ q₁ : word Q d e} :  red_step q₀ q₁ → red_step (p ≫* q₀) (p ≫* q₁) :=
+begin sorry end
+
+
+def quot_comp { c d e : V} (p : quot $ @red_step V Q c d) (q : quot $ @red_step V Q d e) : (quot $ @red_step V Q c e) :=
+begin
+  fapply quot.lift_on p,
+  fapply quot.lift_on q,
+  { rintro f g, exact quot.mk _ (g ≫* f),},
+  { rintro f f' redf, apply funext, rintro g, apply quot.sound, apply red_step.append_right_congr redf, },
+  { rintro f f' redf, apply quot.induction_on q, 
+    { rintro, apply quot.sound, apply red_step.append_left_congr redf, }, },
+end
+
+def quot_comp' { c d e : V} (p : quot $ @red_step V Q c d) (q : quot $ @red_step V Q d e) : (quot $ @red_step V Q c e) :=
+begin
+  fapply quot.lift_on p,
+  fapply quot.lift_on q,
+  { rintro f g, exact quot.mk _ (g ≫* f),},
+  { rintro f f' redf, apply funext, rintro g, apply quot.sound, apply red_step.append_right_congr redf, },
+  { rintro f f' redf, apply quot.induction_on q, 
+    rintro, apply quot.sound, apply red_step.append_left_congr redf, },
+end
+
+def quot_inv {c d : V} (p : quot $ @red_step V Q c d) : (quot $ @red_step V Q d c) :=
+begin
+  fapply quot.lift_on p,
+  { rintro f, exact quot.mk _ (f.reverse), },
+  { rintro f f' redf, apply quot.sound, simp only [red_step.reverse], exact redf, }
+end
+
+lemma id_quot_comp { c d : V} (p : quot $ @red_step V Q c d) : quot_comp (quot.mk _ (@word.nil V Q c)) p = p :=
+begin
+  apply quot.induction_on p,
+  rintro, apply quot.eqv_gen_sound,
+  simp only [word.nil_append],
+  apply eqv_gen.refl,
+end
+
+lemma quot_comp_id { c d : V} (p : quot $ @red_step V Q c d) : quot_comp p (quot.mk _ (@word.nil V Q d)) = p :=
+begin
+  apply quot.induction_on p,
+  rintro, apply quot.eqv_gen_sound,
+  simp only [word.append_nil],
+  apply eqv_gen.refl,
+end
+
+lemma quot_comp_assoc { c d e f : V} 
+  (p : quot $ @red_step V Q c d) (q : quot $ @red_step V Q d e)  (r : quot $ @red_step V Q e f) :
+  quot_comp (quot_comp p q) r = quot_comp p (quot_comp q r) :=
+begin
+  apply quot.induction_on p,
+  apply quot.induction_on q,
+  apply quot.induction_on r,
+  rintro pp qq rr,
+  dsimp only [quot_comp], simp only [quot.lift_on_mk, word.append_assoc],
+end
+
+lemma quot_inv_comp { c d : V} (p : quot $ @red_step V Q c d)  : quot_comp p (quot_inv p) = quot.mk _ (@word.nil V Q c) :=
+begin
+  apply quot.induction_on p,
+  rintro,
+  dsimp only [quot_comp, quot_inv], 
+  simp only [quot.lift_on_mk], 
+  apply quot.eqv_gen_sound,
+  sorry,
+end
+
+lemma quot_comp_inv { c d : V} (p : quot $ @red_step V Q c d)  : quot_comp (quot_inv p) p = quot.mk _ (@word.nil V Q d) :=
+begin
+  apply quot.induction_on p,
+  rintro,
+  dsimp only [quot_comp, quot_inv], 
+  simp only [quot.lift_on_mk], 
+  apply quot.eqv_gen_sound,
+  sorry,
 end
 
 
-lemma word.reverse_reduce_eq_reduce_reverse : Π {c d : V} (w : word Q c d), w.reverse.reduce = w.reduce.reverse
-| _ _ (word.nil) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced,word.reverse], simp}
-| _ _ (word.letter_p _) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], simp,}
-| _ _ (word.letter_n _) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], simp,}
-| _ _ (@word.cons_p _ _ x y z p (@word.cons_n _ _ .(y) w .(z) q tail)) := by 
-  { dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], 
-    sorry }
-| _ _ (@word.cons_n _ _ x y z p (@word.cons_p _ _ .(y) w .(z) q tail)) :=
-  by 
-  { dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry, }  -- h₁ should be a contradiction, but not sure of the proper way to get it
-| _ _ (word.cons_p p (word.cons_p q tail)) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry,}
-| _ _ (word.cons_n p (word.cons_n q tail)) := by {dsimp [word.head_reduce,word.reduce,word.is_head_reduced,word.is_reduced], sorry,}
+def free_groupoid' : groupoid (free_groupoid V) :=
+{ to_category :=
+  { to_category_struct := 
+    { to_quiver := 
+      { hom := λ c d, quot (@red_step V Q c d) }
+    , id := λ a, quot.mk _ (@word.nil V Q a)
+    , comp := λ a b c p q, quot_comp p q }
+  , id_comp' := λ a b p, id_quot_comp p
+  , comp_id' := λ a b p, quot_comp_id p
+  , assoc' := λ a b c d p q r, quot_comp_assoc p q r }
+, inv := λ a b p, quot_inv p
+, inv_comp' := λ a b p, (quot_comp_inv p)
+, comp_inv' := λ a b p, quot_inv_comp p }
 
-lemma word.reduce_both_append_reduce : Π {c d e : V} (u : word Q c d) (w : word Q d e), (u↓ ≫* w↓)↓ = (u ≫* w)↓ := sorry
-lemma word.reduce_left_append_reduce : Π {c d e : V} (u : word Q c d) (w : word Q d e), (u↓ ≫* w)↓ = (u ≫* w)↓ := sorry
-lemma word.reduce_right_append_reduce : Π {c d e : V} (u : word Q c d) (w : word Q d e), (u ≫* w↓)↓ = (u ≫* w)↓ := sorry
-
-local infix ` ≫↓ `:50 := λ {c d e: V} (u : word Q c d) (w : word Q d e), (u ≫* w)↓ 
-
-
--- follows from the three lemmas above plus associativity of plain `append`
-lemma word.append_reduce.assoc : Π {c d e f : V} (u : word Q c d) (w : word Q d e) (v : word Q e f), (u ≫↓ w) ≫↓ v = (u ≫↓ (w ≫↓ v)) := sorry
-
-instance groupoid {w }
 
 end free
 end groupoid
